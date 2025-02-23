79697e5d55ad73ef2ea7cccd02c91dd4eec8c6b3
Jonathan Ho <jonafanho@gmail.com>
2025-02-22 18:48:03 -0800

    Added a setup script

---
+++ setup.bat
@@ -0,0 +1,80 @@
+del mods\* /q
+
+packwiz curseforge add -y plan-player-analytics
+packwiz modrinth add -y ad-astra
+packwiz modrinth add -y ad-astra-delight
+packwiz modrinth add -y ae2
+packwiz modrinth add -y amendments
+packwiz modrinth add -y animationoverhaul
+packwiz modrinth add -y anvil-fix-reborn
+packwiz modrinth add -y better-end-cities-for-betterend
+packwiz modrinth add -y betterend
+packwiz modrinth add -y betterend-delight
+packwiz modrinth add -y betternether
+packwiz modrinth add -y betternether-delight
+packwiz modrinth add -y botania
+packwiz modrinth add -y botania-delight
+packwiz modrinth add -y carpet
+packwiz modrinth add -y continuity
+packwiz modrinth add -y ct-overhaul-village
+packwiz modrinth add -y ctov-farmers-delight-compat
+packwiz modrinth add -y cultural-delights-fabric
+packwiz modrinth add -y cyberanner-ironchest
+packwiz modrinth add -y entityculling
+packwiz modrinth add -y environmental-creepers
+packwiz modrinth add -y expanded-delight
+packwiz modrinth add -y farmers-delight-refabricated
+packwiz modrinth add -y ferrite-core
+packwiz modrinth add -y indium
+packwiz modrinth add -y inventory-management
+packwiz modrinth add -y iris
+packwiz modrinth add -y jade
+packwiz modrinth add -y jade-addons-fabric
+packwiz modrinth add -y jei
+packwiz modrinth add -y just-enough-effect-descriptions-jeed
+packwiz modrinth add -y just-enough-professions-jep
+packwiz modrinth add -y just-enough-resources-jer
+packwiz modrinth add -y justenoughbreeding
+packwiz modrinth add -y lambdynamiclights
+packwiz modrinth add -y lithium
+packwiz modrinth add -y memoryleakfix
+packwiz modrinth add -y minecraft-transit-railway
+packwiz modrinth add -y modern-industrialization
+packwiz modrinth add -y modmenu
+packwiz modrinth add -y naturalist
+packwiz modrinth add -y natures-compass
+packwiz modrinth add -y no-chat-reports
+packwiz modrinth add -y ore-harvester
+packwiz modrinth add -y smarter-farmers-farmers-replant
+packwiz modrinth add -y sodium
+packwiz modrinth add -y sodium-extra
+packwiz modrinth add -y sound-physics-remastered
+packwiz modrinth add -y structory
+packwiz modrinth add -y structory-towers
+packwiz modrinth add -y supplementaries
+packwiz modrinth add -y tectonic
+packwiz modrinth add -y terralith
+packwiz modrinth add -y tree-harvester
+packwiz modrinth add -y villager-names-serilum
+packwiz modrinth add -y when-dungeons-arise
+packwiz modrinth add -y when-dungeons-arise-seven-seas
+packwiz modrinth add -y xaeros-minimap
+packwiz modrinth add -y xaeros-world-map
+packwiz modrinth add -y yungs-better-desert-temples
+packwiz modrinth add -y yungs-better-dungeons
+packwiz modrinth add -y yungs-better-end-island
+packwiz modrinth add -y yungs-better-jungle-temples
+packwiz modrinth add -y yungs-better-mineshafts
+packwiz modrinth add -y yungs-better-nether-fortresses
+packwiz modrinth add -y yungs-better-ocean-monuments
+packwiz modrinth add -y yungs-better-strongholds
+packwiz modrinth add -y yungs-better-witch-huts
+packwiz modrinth add -y yungs-bridges
+packwiz modrinth add -y yungs-cave-biomes
+packwiz modrinth add -y yungs-extras
+packwiz modrinth add -y zoomify
+
+del mods\farmers-delight-fabric.pw.toml
+packwiz refresh
+
+pause
