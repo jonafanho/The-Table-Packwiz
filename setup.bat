9721c5c1f94e4744519a62cab1280f6d9bbc2516
Jonathan Ho <jonafanho@gmail.com>
2025-02-24 13:26:33 -0800

    Changed the tree chopping and vein mining mods again

--- setup.bat
+++ setup.bat
@@ -30,11 +30,11 @@ packwiz modrinth add -y entityculling
 packwiz modrinth add -y environmental-creepers
 packwiz modrinth add -y expanded-delight
 packwiz modrinth add -y extra-mod-integrations
-packwiz modrinth add -y fallingtree
 packwiz modrinth add -y farmers-delight-refabricated
 packwiz modrinth add -y ferrite-core
 packwiz modrinth add -y indium
 packwiz modrinth add -y inventory-management
+packwiz modrinth add -y inventory-sorting
 packwiz modrinth add -y iris
 packwiz modrinth add -y ironchests
 packwiz modrinth add -y jade
@@ -55,6 +55,7 @@ packwiz modrinth add -y naturalist
 packwiz modrinth add -y natures-compass
 packwiz modrinth add -y no-chat-reports
 packwiz modrinth add -y no-enderman-grief
+packwiz modrinth add -y ore-harvester
 packwiz modrinth add -y smarter-farmers-farmers-replant
 packwiz modrinth add -y sodium
 packwiz modrinth add -y sodium-extra
@@ -64,6 +65,7 @@ packwiz modrinth add -y structory-towers
 packwiz modrinth add -y supplementaries
 packwiz modrinth add -y tectonic
 packwiz modrinth add -y terralith
+packwiz modrinth add -y tree-harvester
 packwiz modrinth add -y villager-names-serilum
 packwiz modrinth add -y xaeros-minimap
 packwiz modrinth add -y xaeros-world-map
